6b394305c78235c7e22b0713ac0495217b1898a4
matheuslmarchetti <lunguinhomarchetti@gmail.com>
2023-05-15 16:37:15 -0400

    Desenvolvimento poke-api.js

---
+++ pokedex/assets/js/poke-api.js
@@ -0,0 +1,27 @@
+const pokeApi = {};
+
+function convertPokeApiDetailToPOkemon(pokeDetail) {
+    const pokemon = new Pokemon();
+    pokemon.number = pokeDetail.id;
+    pokemon.name = pokeDetail.name;
+
+    const types = pokeDetail.types.map((typeSlot) => typeSlot.type.name);
+    const [type] = types;
+
+    pokemon.types = type;
+    pokemon.type = type;
+
+    pokemon.photo = pokeDetail.sprites.other.dream_world.font_default
+
+    return pokemon
+}
+
+pokeApi.getPokemonDetail = (pokemon) => {
+    return fetch(pokemon.url).then((response) => response.json()).then(convertPokeApiDetailToPOkemon)
+}
+
+pokeApi.getPokemons = (offset = 0, limit = 5) => {
+    const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`
+
+    return fetch(url).then((response) => response.json()).then((jsonBody) => jsonBody.results).then((pokemons) => pokemons.map(pokeApi.getPokemonDetail)).then((detailRequests) => Promise.all(detailRequests)).then((pokemonsDetails) => pokemonsDetails)
+}
