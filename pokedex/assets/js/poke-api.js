f98d2c46b9edda6a1a918461afb5edc9e8950ef4
matheuslmarchetti <lunguinhomarchetti@gmail.com>
2023-05-15 16:47:01 -0400

    resolução de bugs

--- pokedex/assets/js/poke-api.js
+++ pokedex/assets/js/poke-api.js
@@ -1,29 +1,35 @@
-const pokeApi = {};
 
-function convertPokeApiDetailToPOkemon(pokeDetail) {
-    const pokemon = new Pokemon();
-    pokemon.number = pokeDetail.id;
-    pokemon.name = pokeDetail.name;
+const pokeApi = {}
 
-    const types = pokeDetail.types.map((typeSlot) => typeSlot.type.name);
-    const [type] = types;
+function convertPokeApiDetailToPokemon(pokeDetail) {
+    const pokemon = new Pokemon()
+    pokemon.number = pokeDetail.id
+    pokemon.name = pokeDetail.name
 
-    pokemon.types = type;
-    pokemon.type = type;
+    const types = pokeDetail.types.map((typeSlot) => typeSlot.type.name)
+    const [type] = types
 
-    pokemon.photo = pokeDetail.sprites.other.dream_world.font_default
+    pokemon.types = types
+    pokemon.type = type
+
+    pokemon.photo = pokeDetail.sprites.other.dream_world.front_default
 
     return pokemon
 }
 
 pokeApi.getPokemonDetail = (pokemon) => {
     return fetch(pokemon.url)
-    .then((response) => response.json())
-    .then(convertPokeApiDetailToPokemon)
+        .then((response) => response.json())
+        .then(convertPokeApiDetailToPokemon)
 }
 
 pokeApi.getPokemons = (offset = 0, limit = 5) => {
     const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`
 
-    return fetch(url).then((response) => response.json()).then((jsonBody) => jsonBody.results).then((pokemons) => pokemons.map(pokeApi.getPokemonDetail)).then((detailRequests) => Promise.all(detailRequests)).then((pokemonsDetails) => pokemonsDetails)
+    return fetch(url)
+        .then((response) => response.json())
+        .then((jsonBody) => jsonBody.results)
+        .then((pokemons) => pokemons.map(pokeApi.getPokemonDetail))
+        .then((detailRequests) => Promise.all(detailRequests))
+        .then((pokemonsDetails) => pokemonsDetails)
 }
