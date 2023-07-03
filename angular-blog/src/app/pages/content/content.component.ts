3a19cb16555b15929631f154a3420ad880970caa
matheuslmarchetti <lunguinhomarchetti@gmail.com>
2023-07-03 16:25:38 -0400

    Desenvolvimento de páginas e conteúdos dinâmicos.

--- angular-blog/src/app/pages/content/content.component.ts
+++ angular-blog/src/app/pages/content/content.component.ts
@@ -1,12 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { dataFake } from '../../data/dataFake';
 
 @Component({
   selector: 'app-content',
   templateUrl: './content.component.html',
   styleUrls: ['./content.component.css']
 })
-export class ContentComponent {
-  photoCover:string = 'https://source.unsplash.com/random/?technology';
+export class ContentComponent implements OnInit {
+  photoCover:string = '';
   contentTitle:string = 'Lorem ipsum dolor sit amet consectetur adipisicing elit.';
   contentDescription:string = 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Repudiandae nobis nisi sint accusamus autem eaque voluptatem iste ratione distinctio veniam quisquam, quam, dolorum esse facilis iure commodi cupiditate nesciunt dolores!';
+  private id: string | null = "0";
+
+  constructor (private route:ActivatedRoute) {
+
+  }
+  ngOnInit(): void {
+    this.route.paramMap.subscribe( value =>
+      this.id = value.get("id")
+    )
+
+    this.setValuesToComponent(this.id)
+  }
+
+  setValuesToComponent(id:string | null) {
+    const result = dataFake.filter(article => article.id == id)[0]
+
+    this.contentTitle = result.title
+    this.contentDescription = result.description
+    this.photoCover = result.photoCover
+  }
 }
