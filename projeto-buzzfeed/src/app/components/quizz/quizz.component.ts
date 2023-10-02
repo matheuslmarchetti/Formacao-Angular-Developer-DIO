8e6dd33f32ddbb4fa2441e9ac2798d973310a95f
matheuslmarchetti <lunguinhomarchetti@gmail.com>
2023-10-02 13:55:38 -0400

    Criando regras do jogo do projeto

--- projeto-buzzfeed/src/app/components/quizz/quizz.component.ts
+++ projeto-buzzfeed/src/app/components/quizz/quizz.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import quizz_questions from "../../../assets/data/quizz_questions.json"
 
 @Component({
   selector: 'app-quizz',
@@ -23,7 +24,30 @@ export class QuizzComponent implements OnInit {
   constructor(){}
 
   ngOnInit(): void {
-    throw new Error('Method not implemented.');
+    if(quizz_questions){
+      this.finished = false
+      this.title = quizz_questions.title
+
+      this.questions = quizz_questions.questions
+      this.questionsSelected = this.questions[this.questionIndex]
+
+      this.questionIndex = 0
+      this.questionMaxIndex = this.questions.length
+    }
+  }
+
+  playerChoose(value:string){
+    this.answers.push(value)
+  }
+
+  async nextStep(){
+    this.questionIndex += 1
+
+    if (this.questionMaxIndex > this.questionIndex) {
+      this.questionsSelected = this.questions[this.questionIndex]
+    } else {
+      this.finished = true
+    }
   }
 
 }
