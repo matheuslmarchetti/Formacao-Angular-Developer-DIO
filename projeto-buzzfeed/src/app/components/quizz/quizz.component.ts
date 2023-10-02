47dd9dc708411152ececf959457119459565e400
matheuslmarchetti <lunguinhomarchetti@gmail.com>
2023-10-02 15:03:26 -0400

    Calculando resultado do projeto

--- projeto-buzzfeed/src/app/components/quizz/quizz.component.ts
+++ projeto-buzzfeed/src/app/components/quizz/quizz.component.ts
@@ -38,6 +38,7 @@ export class QuizzComponent implements OnInit {
 
   playerChoose(value:string){
     this.answers.push(value)
+    this.nextStep()
   }
 
   async nextStep(){
@@ -46,8 +47,23 @@ export class QuizzComponent implements OnInit {
     if (this.questionMaxIndex > this.questionIndex) {
       this.questionsSelected = this.questions[this.questionIndex]
     } else {
+      const finalAnswer:string = await this.checkResult(this.answers)
       this.finished = true
+      this.answersSelected = quizz_questions.results[finalAnswer as keyof typeof quizz_questions.results]
     }
   }
 
+  async checkResult(answers:string[]){
+    const result = answers.reduce((previous, current, i, arr)=>{
+      if (
+        arr.filter(item => item === previous).length >
+        arr.filter(item => item === current).length
+      ) {
+        return previous
+      } else {
+        return current
+      }
+    })
+    return result
+  }
 }
