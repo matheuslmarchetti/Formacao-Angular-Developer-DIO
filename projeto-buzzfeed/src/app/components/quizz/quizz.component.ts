b89552b26b94225b410e32942d1fff145a24435b
matheuslmarchetti <lunguinhomarchetti@gmail.com>
2023-10-02 13:27:18 -0400

    Criando propriedades lógicas do projeto

--- projeto-buzzfeed/src/app/components/quizz/quizz.component.ts
+++ projeto-buzzfeed/src/app/components/quizz/quizz.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-quizz',
   templateUrl: './quizz.component.html',
   styleUrls: ['./quizz.component.css']
 })
-export class QuizzComponent {
+export class QuizzComponent implements OnInit {
+  
+  title:string = ""
+
+  questions:any
+  questionsSelected:any
+  
+  answers:string[] = []
+  answersSelected:string = ""
+
+  questionIndex:number = 0
+  questionMaxIndex:number = 0
+
+  finished:boolean = false
+
+  constructor(){}
+
+  ngOnInit(): void {
+    throw new Error('Method not implemented.');
+  }
 
 }
